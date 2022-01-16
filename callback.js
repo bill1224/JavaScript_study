1d307006530f2424895257ced365dda6346db599
jongin1004 <bill1224@naver.com>
2022-01-16 22:20:54 +0900

    promise

--- callback.js
+++ callback.js
@@ -1,36 +1,33 @@
 class UserStorage {
-    loginUser(id, password, onSuccess, onError) {
-        setTimeout(() => {
-            if (
-                (id === 'jongin' && password === 'code')
-            ) {
-                onSuccess(id);
-            } else {
-                onError(new Error('not found'));
-            }
-        }, 2000);
+    loginUser(id, password) {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                if ((id === 'jongin' && password === 'code')) {
+                    resolve(id);
+                } else {
+                    reject(new Error('not found'));
+                }
+            }, 2000);
+        });        
     }
 
-    getRoles(user, onSuccess, onError) {
-        setTimeout(() => {
-            if (user === 'jongin') {
-                onSuccess({ name: 'jongin', role: 'admin'});
-            } else {
-                onError(new Error('not access'));
-            }
-        }, 1000);        
+    getRoles(id) {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                if (id === 'jongin') {
+                    resolve({name: id, role: 'admin'});
+                } else {
+                    reject(new Error('not access'));
+                }
+            }, 1000);
+        });        
     }
 }
 
 let userStorage = new UserStorage();
 let id = prompt('user id');
 let password = prompt('password');
-userStorage.loginUser(id, password, (id) => {
-    userStorage.getRoles(id, (userInfo) => {
-        alert(`name: ${userInfo.name}, role: ${userInfo.role}`);
-    }, (error) => {
-        alert(error);
-    })
-}, (error) => {
-    alert(error);
-})
+userStorage.loginUser(id, password)
+            .then((id) => { return userStorage.getRoles(id) })
+            .then((userInfo) => { alert(`name: ${userInfo.name}, role: ${userInfo.role}`) })
+            .catch((error) => { alert(error) });            
